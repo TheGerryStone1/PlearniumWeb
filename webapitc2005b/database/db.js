c836b69ced19569fa4cd52d8d28dc32fe2f7ebda
Jorge Penagos <A01378450@itesm.mx>
2021-10-13 19:05:28 -0500

    DataBaseUpdate
    cambio en la base de datos

--- webapitc2005b/database/db.js
+++ webapitc2005b/database/db.js
@@ -1,30 +1,17 @@
+// Server=tcp:plearniumserver.database.windows.net,1433;Initial Catalog=PlearniumDB;Persist Security Info=False;User ID=Terny;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;
 const sql = require('mssql')
 
-// Server=tcp:webclass1.database.windows.net,1433;Initial Catalog=web;Persist Security Info=False;User ID=webclass1;Password=tc_2005_b;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;
-
 const config = {
-    user: 'adminEquipo7',
-    password: 'FiestaOwO!',
-    database: 'Plearnium7',
-    server: 'plearnium7.database.windows.net',
-    options: {
-      trustedConnection: true
-    }
-}
-/*const dockerConfig = {
-    user: 'sa',
-    password: 'tc_2005_b',
-    database: 'master',
-    server: 'localhost',
-    port: 1433,
+    user: 'Terny',
+    password: 'Hola1234',
+    database: 'PlearniumDB',
+    server: 'plearniumserver.database.windows.net',
     options: {
     trustedConnection: true
   }
-}*/
+}
 
-let stringConnection = 'Server=tcp:plearnium7.database.windows.net,1433;Initial Catalog=Equipo7;Persist Security Info=False;User ID=adminEquipo7;Password=FiestaOwO!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;'
-let dockerStringConnection = 'Server=localhost,1433;Database=master;User ID=sa;Password=tc_2005_b;'
-const poolPromise = new sql.ConnectionPool(stringConnection)
+const poolPromise = new sql.ConnectionPool(config)
   .connect()
   .then(pool => {
     console.log('Connected to MSSQL')
